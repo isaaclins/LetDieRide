04dd314a1a6bed5dba73329c2aa3afe72b743e07
Isaac Lins <contact@isaaclins.com>
2026-02-21 18:51:01 +0100

    Update window settings to set a fixed resolution of 1280x720 while maintaining resizability and minimum dimensions.

--- settings/window.lua
+++ settings/window.lua
@@ -1,3 +1,3 @@
 love.window.setIcon(love.image.newImageData("content/icon/icon.png"))
 love.window.setTitle("Dice X Balatro")
-love.window.setMode(0,0, { resizable = true, minwidth = 100, minheight = 100 })
+love.window.setMode(1280,720, { highdpi = true, resizable = true, minwidth = 100, minheight = 100 })
